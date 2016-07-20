e9df9cf8a0e7e25dd7db97a75dab1c5dccbdb5e5
kurema <kurema_makoto_software@yahoo.co.jp>
2016-07-20 12:11:25 +0900

    Initial

---
+++ MacDictinoaryConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacDictinoaryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic1=(PlistCS.DictionaryOrdered<string, object>)PlistCS.Plist.readPlist("../Info.plist");
+            var t = dic1["IDXDictionaryIndexes"];
+            }
+    }
+}
---
+++ MacDictionaryGeneral/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacDictionaryGeneral
+{
+    public class PlistReader
+    {
+        public void Read(string path)
+        {
+            var dic=PlistCS.Plist.readPlist(path);
+        }
+    }
+}
