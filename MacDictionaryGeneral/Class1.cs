f1bffa90bf137984bb27775fb282f48315310e68
kurema <kurema_makoto_software@yahoo.co.jp>
2016-07-20 16:46:27 +0900

    quick save
    contains error.

--- MacDictionaryGeneral/Class1.cs
+++ MacDictionaryGeneral/Class1.cs
@@ -4,13 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace MacDictionaryGeneral
 {
     public class GeneralObjectReader
     {
-        public void SetPlist(Dictionary<string, object> info)
+        public static Dictionary<string,string> LoadSingleEntry(Stream sr,Dictionary<string, object> info)
         {
+            bool BigEndien = (bool)info["IDXIndexBigEndian"];
+            var entryBinary = Functions.LoadBytes(sr, 4, BigEndien);
 
+            var sr2 = new MemoryStream(entryBinary);
+            if (info.ContainsKey("TrieAuxiliaryDataOptions"))
+            {
+                var dataOption = (Dictionary<string, object>)info["TrieAuxiliaryDataOptions"];
+            }    
+        }
+
+        public interface IEntry
+        {
+            byte[] Content { get; }
+            long Address { get; }
+
+            string ToString(System.Text.Encoding enc);
+        }
+
+        public class Entry:IEntry
+        {
+            public byte[] Content { get; private set; }
+            public long Address { get; private set; }
+
+            public Entry(Stream sr, bool BigEndien)
+            {
+                Address = sr.Position;
+
+                byte[] bytes = new byte[4];
+
+                sr.Read(bytes, 0, 4);
+                int rawLen = Functions.UnpackInt(bytes, BigEndien);
+
+                var data = new byte[rawLen];
+                sr.Read(data, 0, rawLen);
+                this.Content = data;
+            }
+
+            public static Entry[] GetEntries(Stream sr, bool BigEndien)
+            {
+                List<Entry> result = new List<Entry>();
+                while (true)
+                {
+                    result.Add(new Entry(sr, BigEndien));
+                    if (sr.Position >= sr.Length) break;
+                }
+                return result.ToArray();
+            }
+
+            public string ToString(Encoding enc)
+            {
+                return enc.GetString(Content);
+            }
+        }
+
+        public class EntryCompressed:IEntry
+        {
+            public byte[] Content { get { return _Content == null ? _Content = Functions.Decompress(Compressed, RawLength) : _Content; } }
+
+            public long Address { get; private set; }
+
+            private byte[] _Content = null;
+            private byte[] Compressed;
+            private int RawLength;
+
+            public EntryCompressed(Stream sr,bool BigEndien)
+            {
+                Address = sr.Position;
+
+            }
+
+            public static EntryCompressed[] GetEntries(Stream sr, bool BigEndien)
+            {
+                List<EntryCompressed> result = new List<EntryCompressed>();
+                while (true)
+                {
+                    result.Add(new EntryCompressed(sr, BigEndien));
+                    if (sr.Position >= sr.Length) break;
+                }
+                return result.ToArray();
+            }
+
+            public string ToString(Encoding enc)
+            {
+                return enc.GetString(Content);
+            }
         }
     }
 
---
+++ MacDictionaryGeneral/Functions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace MacDictionaryGeneral
+{
+    public static class Functions
+    {
+        public static int UnpackInt(byte[] bytes,bool BigEndien)
+        {
+            if (BitConverter.IsLittleEndian == BigEndien)
+                Array.Reverse(bytes);
+
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public static uint UnpackUInt(byte[] bytes, bool BigEndien)
+        {
+            if (BitConverter.IsLittleEndian == BigEndien)
+                Array.Reverse(bytes);
+
+            return BitConverter.ToUInt32(bytes, 0);
+        }
+
+        public static short UnpackShort(byte[] bytes, bool BigEndien)
+        {
+            if (BitConverter.IsLittleEndian == BigEndien)
+                Array.Reverse(bytes);
+
+            return BitConverter.ToInt16(bytes, 0);
+        }
+
+        public static ushort UnpackUShort(byte[] bytes, bool BigEndien)
+        {
+            if (BitConverter.IsLittleEndian == BigEndien)
+                Array.Reverse(bytes);
+
+            return BitConverter.ToUInt16(bytes, 0);
+        }
+
+        public static byte[] LoadBytes(Stream sr,int SizeLength,bool BigEndien)
+        {
+            Int64 Size = 0;
+            Int64 Base = 1;
+            for(int i = 0; i < SizeLength; i++)
+            {
+                var temp = sr.ReadByte();
+                if (BigEndien)
+                {
+                    Size = Size * 0x100 + temp;
+                }
+                else
+                {
+                    Size += Base * temp;
+                    Base *= 0x100;
+                }
+            }
+            if (Size > int.MaxValue) { throw new Exception(); }
+            var result = new byte[Size];
+            sr.Read(result, 0, (int)Size);
+            return result;
+        }
+
+        public static byte[] Decompress(Stream sr, bool BigEndien)
+        {
+            byte[] bytes = new byte[4];
+
+            sr.Read(bytes, 0, 4);
+            int compdLen = Functions.UnpackInt(bytes, BigEndien);
+            var data = new byte[compdLen - 4];
+            sr.Read(data, 0, compdLen - 4);
+
+            return Decompress(data, BigEndien);
+        }
+
+        public static byte[] Decompress(byte[] Origin, bool BigEndien)
+        {
+            var stream = new MemoryStream(Origin);
+
+            byte[] bytes = new byte[4];
+            stream.Read(bytes, 0, 4);
+            int rawLen = Functions.UnpackInt(bytes, BigEndien);
+
+            stream.Seek(2, SeekOrigin.Begin);
+            using (var ds = new System.IO.Compression.DeflateStream(stream, System.IO.Compression.CompressionMode.Decompress))
+            {
+                ds.Seek(0, SeekOrigin.Begin);
+                var result = new byte[rawLen];
+                ds.Read(result, 0, rawLen);
+                return result;
+            }
+        }
+    }
+}
